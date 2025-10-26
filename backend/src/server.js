2a391bc1807c39908bf082f28ad921279f0b861f
abhimanyu012 <abhimanyucode2021@gmail.com>
2025-10-26 16:59:55 +0530

    feat: initialize package.json and set up server configuration

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,24 @@
 import express from 'express'
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/messages.route.js';
-const app = express();
 import * as dotenv from 'dotenv';
+import path from 'path'
 dotenv.config();
-
-app.use(express.json())
-
-const PORT = process.env.PORT || 5000
+const app = express();
+const PORT = process.env.PORT || 3000
+const __dirname = path.resolve()
 
 app.use("/api/auth", authRoutes)
-app.use("/api/messages", messageRoutes )
+app.use("/api/messages", messageRoutes)
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+  // Use a RegExp to avoid path-to-regexp parameter parsing issues (matches all routes)
+  app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+  })
+}
 
 
 app.listen(PORT, () => {
